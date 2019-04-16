0ade675f101593ca44508dce73010e2a9a9b7e7c
Myriam Boran <brnmyriam45@googlemail.com>
2019-04-16 13:37:46 +0100

    lab

---
+++ client/src/views/sighting_form_view.js
@@ -0,0 +1,18 @@
+const PubSub = require('../helpers/pub_sub.js')
+
+const SightingFormView = function (form) {
+  this.form = form;
+};
+
+SightingFormView.prototype.bindEvents = function () {
+  this.form.addEventListener('submit', (evt) => {
+    this.handleSubmit(evt);
+  });
+};
+
+SightingFormView.prototype.handleSubmit = function (evt) {
+  evt.preventDefault();
+  PubSub.publish('FormView:form-submitted', evt.target);
+}
+
+module.exports = SightingFormView;
